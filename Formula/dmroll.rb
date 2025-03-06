539eea8d71b29f710a80e28dd7fc153b3201951f
ADWrobo <adwroblewski@icloud.com>
2025-03-05 18:11:11 -0600

    Does install with aliases depending on version

--- Formula/dmroll.rb
+++ Formula/dmroll.rb
@@ -7,19 +7,24 @@ class Dmroll < Formula
       if Hardware::CPU.arm?
         url "https://github.com/ADWrobo/dmroll/releases/download/v0.1.0/dmroll-arm64"
         sha256 "1d97594d10eb5e0a031efecb35e78656a37355a78192f0681543c4b385f66fde"
+        def install
+            bin.install "dmroll-arm64" => "dmroll"
+        end
       else
         url "https://github.com/ADWrobo/dmroll/releases/download/v0.1.0/dmroll"
         sha256 "93ab3ede68a179fdf9af9373cf6ff3919f80d85fe6a56cc09a4766021ab668cc"
+        def install
+            bin.install "dmroll"
+        end
       end
     end
   
     on_linux do
       url "https://github.com/ADWrobo/dmroll/releases/download/v0.1.0/dmroll-linux"
       sha256 "1a06498d6709d10243ca1b09a557f57db34f4c34ad2e1be9246e7417a50f1a85"
-    end
-  
-    def install
-      bin.install "dmroll"
+      def install
+        bin.install "dmroll-linux" => "dmroll"
+      end
     end
   
     test do
